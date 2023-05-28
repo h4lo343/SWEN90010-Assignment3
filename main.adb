9b2816c8975ef9a8dc4357bf50182aa19823dccb
h4lo343 <xiangnan1@student.unimelb.edu.au>
2023-05-28 16:48:17 +1000

    fix/fix-divide-minus-1-bug

--- main.adb
+++ main.adb
@@ -348,7 +348,9 @@ begin
              
              if(IntTemp2 = 0) then Put_Line(Divide_By_Zero); return; end if;
              if (IntTemp1 = 0) then Stack.Push(OperandStack, 0); end if;
-             if (IntTemp1 = Integer'First and IntTemp2 < 0) then return; end if;
+             if (IntTemp1 = Integer'First and IntTemp2 = -1 ) then 
+              Put_Line(Overflow_Occur); 
+              return;
              if (IntTemp1 > 0 and IntTemp2 > 0) then   
                         Bound := IntTemp1;
                         while IntTemp1 >= IntTemp2 and Result < Bound loop
