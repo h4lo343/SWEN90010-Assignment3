10f32cd8c3ae19ac04340b48e4d4ec70f48de710
h4lo343 <xiangnan1@student.unimelb.edu.au>
2023-05-28 19:13:16 +1000

    fix/fix-obj-configure

--- calcularotoperator.adb
+++ calcularotoperator.adb
@@ -1,8 +1,7 @@
-with PIN;
 
-procedure calcularotoperator is
-begin
-   procedure Unlock(Lock_State : out Lock_State_Type, Master_Pin : PIN.PIN, User_Key : PIN.PIN ) is
+package body calcularotoperator is
+
+   procedure Unlock(Lock_State : out Lock_State_Type; Master_Pin : PIN.PIN; User_Key : PIN.PIN ) is
    begin
 
    end Unlock;
